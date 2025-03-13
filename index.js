7cd6c22b08f4888a050ccd1893db974aef182e3d
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-13 14:47:54 +0300

    drivers

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+// index.js
+
+function findMatching(drivers, searchString) {
+    const lowerSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => 
+        driver.toLowerCase() === lowerSearchString
+    );
+}
+
+function fuzzyMatch(drivers, searchString) {
+    const lowerSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => 
+        driver.toLowerCase().startsWith(lowerSearchString)
+    );
+}
+
+function matchName(drivers, searchString) {
+    const lowerSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => 
+        driver.name.toLowerCase() === lowerSearchString
+    );
+}
+
+ 
+module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+};
